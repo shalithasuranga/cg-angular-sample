10f49afb8aece383fd83c5d0889a172a94602d9b
Shalitha Suranga <shalithasuranga@gmail.com>
2019-08-18 19:32:49 +0530

    Add some unit tests for service

--- src/app/components/leaderboard/leaderboard.component.spec.ts
+++ src/app/components/leaderboard/leaderboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LeaderboardComponent } from './leaderboard.component';
+import { FormsModule } from '@angular/forms';
 
 describe('LeaderboardComponent', () => {
   let component: LeaderboardComponent;
@@ -8,7 +9,8 @@ describe('LeaderboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LeaderboardComponent ]
+      declarations: [ LeaderboardComponent ],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
